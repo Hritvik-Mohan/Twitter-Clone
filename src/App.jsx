f1849346071da48a1c1868fa5b5006ad82aa9d46
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-16 23:33:07 +0530

    feat Added chakra ui, react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Feed from './components/Feed/Feed'
 import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Signup from './components/Signup/Signup'
+import Login from './components/Login/Login'
+import { ChakraProvider, Container } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      <Feed />
+      <ChakraProvider>
+        <Container>
+          {/* <Navbar /> */}
+          <BrowserRouter>
+            <Routes>
+              {/* <Route path='/' element={<Login />}/>
+              <Route path='/signup' element={<Signup />}/> */}
+            </Routes>
+          </BrowserRouter>
+          <Feed />
+        </Container>
+      </ChakraProvider>
     </div>
   )
 }
---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { account } from '../../appwrite/appwriteConfig'
+import { useNavigate } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+
+
+export default function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
